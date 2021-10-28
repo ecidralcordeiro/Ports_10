cfc443fcfc8136408e61542faf91240d674d7a46
Lucca Kominek <komineklucca@gmail.com>
2021-10-28 11:49:21 -0300

    implementação do menu lateral

---
+++ dashboard/dashboard.js
@@ -0,0 +1,94 @@
+function recomendados1() {
+    document.getElementById('ra').style.display='block';
+    document.getElementById('rb').style.display='none';
+    document.getElementById('rc').style.display='none';
+    document.getElementById('rd').style.display='none';
+    document.getElementById('re').style.display='none';
+    
+    document.getElementById('b1').style.backgroundColor='rgb(117, 115, 115)';
+    document.getElementById('b2').style.backgroundColor='transparent';
+    document.getElementById('b3').style.backgroundColor='transparent';
+    document.getElementById('b4').style.backgroundColor='transparent';
+    document.getElementById('b5').style.backgroundColor='transparent';
+
+}
+function recomendados2() {
+    document.getElementById('ra').style.display='none';
+    document.getElementById('rb').style.display='block';
+    document.getElementById('rc').style.display='none';
+    document.getElementById('rd').style.display='none';
+    document.getElementById('re').style.display='none';
+
+    document.getElementById('b1').style.backgroundColor='transparent';
+    document.getElementById('b2').style.backgroundColor='rgb(117, 115, 115)';
+    document.getElementById('b3').style.backgroundColor='transparent';
+    document.getElementById('b4').style.backgroundColor='transparent';
+    document.getElementById('b5').style.backgroundColor='transparent';
+}
+function recomendados3() {
+    document.getElementById('ra').style.display='none';
+    document.getElementById('rb').style.display='none';
+    document.getElementById('rc').style.display='block';
+    document.getElementById('rd').style.display='none';
+    document.getElementById('re').style.display='none';
+
+    document.getElementById('b1').style.backgroundColor='transparent';
+    document.getElementById('b2').style.backgroundColor='transparent';
+    document.getElementById('b3').style.backgroundColor='rgb(117, 115, 115)';
+    document.getElementById('b4').style.backgroundColor='transparent';
+    document.getElementById('b5').style.backgroundColor='transparent';
+}
+function recomendados4() {
+    document.getElementById('ra').style.display='none';
+    document.getElementById('rb').style.display='none';
+    document.getElementById('rc').style.display='none';
+    document.getElementById('rd').style.display='block';
+    document.getElementById('re').style.display='none';
+
+    document.getElementById('b1').style.backgroundColor='transparent';
+    document.getElementById('b2').style.backgroundColor='transparent';
+    document.getElementById('b3').style.backgroundColor='transparent';
+    document.getElementById('b4').style.backgroundColor='rgb(117, 115, 115)';
+    document.getElementById('b5').style.backgroundColor='transparent';
+}
+function recomendados5() {
+    document.getElementById('ra').style.display='none';
+    document.getElementById('rb').style.display='none';
+    document.getElementById('rc').style.display='none';
+    document.getElementById('rd').style.display='none';
+    document.getElementById('re').style.display='block';
+
+    document.getElementById('b1').style.backgroundColor='transparent';
+    document.getElementById('b2').style.backgroundColor='transparent';
+    document.getElementById('b3').style.backgroundColor='transparent';
+    document.getElementById('b4').style.backgroundColor='transparent';
+    document.getElementById('b5').style.backgroundColor='rgb(117, 115, 115)';
+}
+
+function flechaesquerda() {
+    document.getElementById('ra').style.display='block';
+    document.getElementById('rb').style.display='none';
+    document.getElementById('rc').style.display='none';
+    document.getElementById('rd').style.display='none';
+    document.getElementById('re').style.display='none';
+
+    document.getElementById('b1').style.backgroundColor='rgb(117, 115, 115)';
+    document.getElementById('b2').style.backgroundColor='transparent';
+    document.getElementById('b3').style.backgroundColor='transparent';
+    document.getElementById('b4').style.backgroundColor='transparent';
+    document.getElementById('b5').style.backgroundColor='transparent';
+}
+
+function flechadireita() {
+    document.getElementById('ra').style.display='none';
+    document.getElementById('rb').style.display='none';
+    document.getElementById('rc').style.display='none';
+    document.getElementById('rd').style.display='none';
+    document.getElementById('re').style.display='block';
+
+    document.getElementById('b1').style.backgroundColor='transparent';
+    document.getElementById('b2').style.backgroundColor='transparent';
+    document.getElementById('b3').style.backgroundColor='transparent';
+    document.getElementById('b4').style.backgroundColor='transparent';
+    document.getElementById('b5').style.backgroundColor='rgb(117, 115, 115)';
+}
