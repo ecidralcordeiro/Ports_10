6f4e68ae582a3d224148cba5c42a9b3ec131bb80
Lucca Kominek <komineklucca@gmail.com>
2021-10-29 13:53:37 -0300

    ajustes no cadastro

--- cadastro/cadastro.js
+++ cadastro/cadastro.js
@@ -1,18 +1,47 @@
-function mostra() {
-    var x = document.getElementById("mostra");
-    if (x.style.display === "none") {
-        x.style.display = "block";
+function interesses() {
+    window.location.href = "interesses.html";
+}
+
+function esconde() {
+    document.getElementById('conteudocadastro').style.opacity='0';
+    document.getElementById('conteudocadastro').style.transition='opacity 2s';
+}
+
+function verifica() {
+    var email = document.getElementById('email');
+    var senha = document.getElementById('senha1');
+    var senharep = document.getElementById('senha2');
+    var x = document.getElementById('cadastro');
+
+    if (email.value == "") {
+        email.style.borderColor = 'rgb(175, 51, 51)';
+        x.disabled = true;
+        x.style.cursor='auto';
     } else {
-        x.style.display = "block";
+        email.style.borderColor = 'grey';
+        x.disabled = false;
+        x.style.cursor='pointer';
     }
-}
 
-function desabilita() {
-    var x = document.getElementById("cadastro");
-    x.disabled = true;
-}
+    if (senha.value == "" || senha.value < 8) {
+        senha.style.borderColor = 'rgb(175, 51, 51)';
+        x.disabled = true;
+        x.style.cursor='auto';
+    } else {
+        senha.style.borderColor = 'grey';
+        x.disabled = false;
+        x.style.cursor='pointer';
+    }
 
-function habilita() {
-    var x = document.getElementById("cadastro");
-    x.disabled = false;
+    if (senharep.value == "" || senharep.value < 8 || senha.value != senharep.value) {
+        senha.style.borderColor = 'rgb(175, 51, 51)';
+        senharep.style.borderColor = 'rgb(175, 51, 51)';
+        x.disabled = true;
+        x.style.cursor='auto';
+    } else {
+        senha.style.borderColor = 'grey';
+        senharep.style.borderColor = 'grey';
+        x.disabled = false;
+        x.style.cursor='pointer';
+    }
 }
