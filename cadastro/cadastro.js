36d671ac3f5a2630638e6472e494620f9b611641
Eduardo Cidral Cordeiro <ecidralcordeiro@gmail.com>
2021-11-01 09:15:12 -0300

    Cadastro Novo

---
+++ cadastro/cadastro.js
@@ -0,0 +1,69 @@
+let userValido = false;
+let userSelecionado;
+let nomeValido = false;
+let passwordValido = false;
+let emailValido = false;
+
+function validateUserType() {
+  console.log(userValido);
+  const options = document.querySelectorAll('input[name="userOption"]');
+  for (const option of options) {
+    if (option.checked) {
+      userSelecionado = option.value;
+      userValido = true;
+      console.log(userValido);
+      validateButton();
+      break;
+    }
+  }
+}
+
+function validateName() {
+  let nameInput = document.querySelector("#name");
+  let name = nameInput.value;
+  if (name == ""  || name == " " || name == null || name == undefined) {
+    nomeValido = false;
+    validateButton();
+    nameInput.style.border = "2px solid #FF0F0F";
+  } else {
+    nomeValido = true;
+    validateButton();
+  }
+}
+
+function validateEmail() {
+  let emailInput = document.querySelector("#mail");
+  let email = emailInput.value;
+  if (email == ""  || email == " "  || email == null  || email == undefined) {
+    emailValido = false;
+    document.querySelector("#mail").style.border = "2px solid #FF0F0F";
+    validateButton();
+  } else {
+    emailValido = true;
+    validateButton();
+  }
+}
+
+function validatePassword() {
+  let senha = document.querySelector("#senha").value;
+  let confirmarSenha = document.querySelector("#confirmarSenha").value;
+  let senhasNãoVazias = senha !== "" && confirmarSenha !== "";
+
+  if (senha === confirmarSenha && senhasNãoVazias) {
+    passwordValido = true;
+    validateButton();
+  } else {
+    passwordValido = false;
+    validateButton();
+  }
+}
+
+function validateButton() {
+  if (nomeValido && passwordValido && userValido && emailValido) {
+    document.querySelector("#finish").disabled = false;
+  } else {
+    document.querySelector("#finish").disabled = true;
+  }
+}
+
+function buttonClick() {}
