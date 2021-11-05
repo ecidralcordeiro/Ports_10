1f58d2363897115eff8d12347bf6ccad6fc72836
Lucca Kominek <komineklucca@gmail.com>
2021-11-05 14:27:37 -0300

    Ajustes css

--- perfil/perfil.js
+++ perfil/perfil.js
@@ -10,3 +10,54 @@ document.querySelector("#telefone").innerText = dadosPessoais.number
 document.querySelector("#data").innerText = dadosPessoais.date
 document.querySelector("#descricao").innerText = dadosPessoais.description
 
+function preview() {
+    'use strict'
+
+    let photo = document.getElementById('fotoCapa');
+    let file = document.getElementById('inputCapa');
+
+    photo.addEventListener('click', () => {
+        file.click();
+    });
+
+    file.addEventListener('change', () => {
+
+        if (file.files.length <= 0) {
+            return;
+        }
+
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            photo.src = reader.result;
+        }
+
+        reader.readAsDataURL(file.files[0]);
+    });
+}
+
+function preview2() {
+    'use strict'
+
+    let photo = document.getElementById('fotoPerfil');
+    let file = document.getElementById('inputPerfil');
+
+    photo.addEventListener('click', () => {
+        file.click();
+    });
+
+    file.addEventListener('change', () => {
+
+        if (file.files.length <= 0) {
+            return;
+        }
+
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            photo.src = reader.result;
+        }
+
+        reader.readAsDataURL(file.files[0]);
+    });
+}
